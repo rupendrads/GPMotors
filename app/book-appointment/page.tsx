2db98279cd731356c750537b692d783616c550db
chittaranjan01 <ranjan74feb80@gmail.com>
2025-05-23 16:18:24 +0530

    initial commit - book appointment file

---
+++ app/book-appointment/page.tsx
@@ -0,0 +1,16 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Book Appointment | Car Repair",
+  description: "Book appointment for your car service",
+  keywords: "car, repair, book appointment",
+  metadataBase: new URL("https://localhost:3000/book-appointment"),
+};
+
+export default function BookAppointmentPage() {
+  return (
+    <main>
+      <h1>Book Appointment page</h1>
+    </main>
+  );
+}
