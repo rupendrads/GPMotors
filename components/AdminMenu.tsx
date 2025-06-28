313f6727e14a5ffcb4fdb582246020f1c045e9a2
chittaranjan01 <ranjan74feb80@gmail.com>
2025-06-28 20:16:04 +0530

    Admin menu modification

--- components/AdminMenu.tsx
+++ components/AdminMenu.tsx
@@ -5,17 +5,23 @@ function AdminMenuDetail() {
   return (
     <nav className="relative z-50 w-full">
       <div className="w-full bg-[#d8d8d8] flex items-center justify-between h-16 px-4 lg:px-10 poppins-font">
-        <div className="hidden lg:flex flex-1 justify-start items-center space-x-8">
-          <Link href="/client-detail" className=" hover:text-[#E33C30] px-3 py-2 transition">
+        <div className="hidden lg:flex flex-1 justify-center items-center space-x-8">
+          <Link
+            href="/client-detail"
+            className=" hover:text-[#E33C30] px-3 py-2 transition"
+          >
             Client Detail
           </Link>
-          <Link href="/booking-config" className=" hover:text-[#E33C30] px-3 py-2 transition">
+          <Link
+            href="/booking-config"
+            className=" hover:text-[#E33C30] px-3 py-2 transition"
+          >
             Booking Configuration
           </Link>
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
 export default AdminMenuDetail;
