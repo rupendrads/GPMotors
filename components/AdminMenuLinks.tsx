a8d451d47229b8330795f475e16ae3a2652dcdca
chittaranjan01 <ranjan74feb80@gmail.com>
2025-10-13 18:13:28 +0530

    Admin menu formatting

--- components/AdminMenuLinks.tsx
+++ components/AdminMenuLinks.tsx
@@ -3,41 +3,45 @@ import Link from "next/link";
 
 function AdminMenuLinks({ closeSidebar }: { closeSidebar?: () => void }) {
   return (
-    <div>
-      <Link
-        href="/admin/booking-config"
-        className=" hover:text-[#E33C30] px-3 py-2 transition"
-        onClick={closeSidebar ?? closeSidebar}
-      >
-        Booking Configuration
-      </Link>
-      <Link
-        href="/admin/book-appointment-list"
-        className=" hover:text-[#E33C30] px-3 py-2 transition"
-        onClick={closeSidebar ?? closeSidebar}
-      >
-        Booking List
-      </Link>
-      <Link
-        href="/admin/client-detail-list"
-        className=" hover:text-[#E33C30] px-3 py-2 transition"
-        onClick={closeSidebar ?? closeSidebar}
-      >
-        Client Detail
-      </Link>
-      <Link
-        href="/admin/reminder"
-        className=" hover:text-[#E33C30] px-3 py-2 transition"
-        onClick={closeSidebar ?? closeSidebar}
-      >
-        Appointment Reminder
-      </Link>
-      <Link
-        href="/"
-        className="px-3 py-2 border text-white rounded-lg bg-red-500 ml-4"
-      >
-        <span className="mr-1">🔒</span> Login
-      </Link>
+    <div className="flex-1 flex justify-between">
+      <div>
+        <Link
+          href="/admin/booking-config"
+          className=" hover:text-[#E33C30] px-3 py-2 transition"
+          onClick={closeSidebar ?? closeSidebar}
+        >
+          Booking Configuration
+        </Link>
+        <Link
+          href="/admin/book-appointment-list"
+          className=" hover:text-[#E33C30] px-3 py-2 transition"
+          onClick={closeSidebar ?? closeSidebar}
+        >
+          Booking List
+        </Link>
+        <Link
+          href="/admin/client-detail-list"
+          className=" hover:text-[#E33C30] px-3 py-2 transition"
+          onClick={closeSidebar ?? closeSidebar}
+        >
+          Client Detail
+        </Link>
+        <Link
+          href="/admin/reminder"
+          className=" hover:text-[#E33C30] px-3 py-2 transition"
+          onClick={closeSidebar ?? closeSidebar}
+        >
+          Appointment Reminder
+        </Link>
+      </div>
+      <div>
+        <Link
+          href="/"
+          className="px-3 py-2 border text-white rounded-lg bg-red-500 ml-4"
+        >
+          <span className="mr-1">🔒</span> Login
+        </Link>
+      </div>
     </div>
   );
 }
