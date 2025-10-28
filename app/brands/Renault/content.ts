56ffd160e83f7fcca01610fe5e5b1ef2185e34ba
pravinkolambkar <pravin.kolambkar@gmail.com>
2025-10-28 15:18:13 +0530

    Citroen,Tesla,Renault,Lexus car brand page commit

---
+++ app/brands/Renault/content.ts
@@ -0,0 +1,46 @@
+const Data = {
+    Hero : {
+        image: "/images/Renaultbanner.jpg",
+        title: " Comprehensive Renault Care",
+        paragraph : "Parts Change offers end-to-end Renault servicing including diagnostics, MOT, and genuine part replacements. From Clio to Captur, our technicians ensure your Renault performs efficiently and safely."
+    },
+    WhyUs : {
+        image: "/images/Renaultlogo.png",
+        title: "Why Choose Parts Change",
+        subtitle: "For Your Renault?",
+        paragraph : "We specialise in Renault service plans built for performance and affordability. Every service ensures precision, reliability, and warranty-backed work."
+    },
+    WhyTrustUs :{
+        paragraph:"We fit OEM Renault parts for every service, each with a 2-year warranty."
+    },
+    WhyUsBanner:{
+      brand:"Renault"
+    },
+    FAQ :[
+    {
+      id: 1,
+      question: "What Renault services do you provide?",
+      answer: "We provide full MOT, maintenance, and repair services with OEM-approved parts."
+    },
+    {
+        id: 2,
+        question: "Do you use genuine Renault parts?", 
+        answer: ""
+    },
+    {
+        id: 3,
+        question: "Can I get a free Renault quote?",
+        answer: "Yes! We’ll send you a detailed quote within 24 hours—no hidden costs."
+    },
+    {
+        id: 4,
+        question: "How do I know my Renault needs service?",
+        answer: ""
+    }
+],Banner:{
+  image : "/images/BottomBannerRenault.png",
+  mobileImage: "/images/BottomBannerRenaultMobile.png"
+}
+}
+
+export default Data
